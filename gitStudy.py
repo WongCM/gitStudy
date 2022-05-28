6b496fc17482a15628a1368fcf18bb4219e15a00
krijnwong <wong_cm@126.com>
2022-05-29 02:38:38 +0800

    Update gitStudy.py

--- gitStudy.py
+++ gitStudy.py
@@ -1,1 +1,23 @@
-print('new branch')
+print(1)
+print('I am Krijn Wong')
+print('I am the God')
+
+print('Here is the context submitted third time')
+print('Here is the context submitted forth time')
+print('Here is the context submitted fifth time, but changed in new branch named "new brach"')
+
+print('Here is the context submitted sixth time, but changed in new branch named "hot fix"')
+
+print('Here is the context submitted seventh time')
+
+print('Here is the context submitted eighth time')
+
+print('Now here begin a new branch')
+
+print('Here is the end of new branch')
+
+print('Now here begin a new branch named "new_branch2"')
+
+print('Now here end a new branch named "new_branch2"')
+
+print('new commit')
